81ce0ca23f716001f5fb9423a99fde069e0f446d
Rafael Marcelo Reimberg <159301053+rafaelMarceloReimberg@users.noreply.github.com>
2026-01-15 19:36:43 -0300

    update do formulario

--- cadastro_usuario/cadastro_usuario/src/app/cadastro/cadastro.ts
+++ cadastro_usuario/cadastro_usuario/src/app/cadastro/cadastro.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Usuario } from '../interfaces/usuario.interface';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro',
+  standalone: true,
   imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {
+
+export class Cadastro implements OnInit {
+  
+  usuarios: WritableSignal<Usuario[]> = signal([]);
+  
   formulario = new FormGroup({
     id: new FormControl(),
     nome: new FormControl("",[Validators.required]),
@@ -18,28 +24,61 @@ export class Cadastro {
     senha: new FormControl()
   });
 
-  usuarios: Usuario[] = [];
 
-  criar(){
-    let formValue = this.formulario.value;
-    console.log(formValue);
+  private readonly API_URL =
+    'https://api.etternum.com.br/integracao/Usuario?$select=id,nome,email';
 
-    if(this.formulario.invalid){
+  private readonly TOKEN =
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIgUkVJTUJFUkciLCJqdGkiOiJhOTVkMzMwZi0xYTRiLTQ2YTMtYjVjMC0wZWI5YzlhZjBiY2IiLCJPcmdhbml6YWNhb0lkIjoiMTk3OCIsIlVzZXJJZCI6IjAiLCJleHAiOjE3Njg1MzI5NjYsImlzcyI6Ik1lZGljaW5hRGlyZXRhLkFQSSIsImF1ZCI6Ik1lZGljaW5hRGlyZXRhLkFQSSJ9.68FwtoLDI94xB4vkMvGl4ret7LfoCnJJ-IOTgvIrh2Y';
+
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit(): void {
+        this.buscarUsuarios();
+  }
+
+  buscarUsuarios(): void{
+    const headers = new HttpHeaders({
+      Authorization: this.TOKEN
+    });
+
+    this.http.get<{ value: any[] }>(this.API_URL, { headers}).subscribe({
+      next: (res) => {
+        this.usuarios.set(res.value);
+
+        console.log(this.usuarios);
+      },
+      error: (err) => {
+        console.error('Erro ao buscar usuários', err);
+      }
+    });
+  }
+
+  
+
+  // 🔹 POST LOCAL (simulação de cadastro)
+  criar(): void {
+    if (this.formulario.invalid) {
       return;
     }
-
-    this.usuarios.push({
-      id:formValue.id,
-      nome:formValue.nome!,
-      email:formValue.email!,
-      celular:formValue.celular!,
-      senha:formValue.senha
-    })
+ 
+    const dados = this.formulario.getRawValue();
+ 
+    this.usuarios().push({
+      id: dados.id ?? Date.now(),
+      nome: dados.nome!,
+      email: dados.email!,
+      celular: dados.celular!,
+      senha: dados.senha!
+    });
+ 
     this.formulario.reset();
   }
-
-  excluir(id: number){
-    this.usuarios = this.usuarios.filter(u => u.id !== id);
+ 
+  // 🔹 DELETE LOCAL
+  excluir(id: number): void {
+    let usuarios = this.usuarios().filter(u => u.id !== id);
+    this.usuarios.set(usuarios);
   }
-
 }
+
