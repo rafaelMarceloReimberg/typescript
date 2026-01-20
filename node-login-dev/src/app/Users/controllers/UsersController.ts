4a065cb162d1ff3624b91553afc5f7fcfa320c63
Rafael Marcelo Reimberg <159301053+rafaelMarceloReimberg@users.noreply.github.com>
2026-01-20 14:47:47 -0300

    UP

--- node-login-dev/src/app/Users/controllers/UsersController.ts
+++ node-login-dev/src/app/Users/controllers/UsersController.ts
@@ -2,7 +2,36 @@ import { Request, Response } from 'express'
 
 class UsersController {
   async index(req: Request, res: Response): Promise<Response> {
-    const user = [{ id: '1987', email: 'raaafael.reimberg@hotmail.com' }]
+    const user = [{ 
+      id: "01", 
+      nome: "Rafael Marcelo Reimberg", 
+      idade: 38, 
+      email: 'raaafael.reimberg@hotmail.com', 
+      celular: "(11) 95210-7767",
+      endereco: {
+        rua: "Rua Emmanuel Chabriel",
+        numero: 166,
+        complemento: "b",
+        bairro: "Parque São Paulo",
+        cidade: "São Paulo",
+        estado: "SP"
+      }
+    },
+    { 
+      id: "02", 
+      nome: "Natalia Lobo de Oliveira", 
+      idade: 34, 
+      email: 'nati.somar@hotmail.com', 
+      celular: "(11) 95210-7767",
+      endereco: {
+        rua: "Rua Emmanuel Chabriel",
+        numero: 166,
+        complemento: "b",
+        bairro: "Parque São Paulo",
+        cidade: "São Paulo",
+        estado: "SP"
+      },
+    }]
 
     return res.status(200).json(user)
   }
